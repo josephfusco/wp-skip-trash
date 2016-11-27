1fd7e0b97c2bff560cb80df6912f635bdd0c881e
Joseph Fusco <hello@josephfus.co>
2016-11-27 15:51:04 -0500

    Initial commit

---
+++ instantly-empty-trash.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:    Instantly Empty Trash
+ * Description:    Instantly empty trash for post & pages
+ * Version:        1.0
+ * Author:         Joseph Fusco
+ * Author URI:     http://josephfus.co/
+ * License:        GPLv2 or later
+ * License URI:    http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+define( 'EMPTY_TRASH_DAYS', 0 );
