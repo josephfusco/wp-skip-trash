1898edc67e901b15cb3f3be22bf27bed4c0dafb6
Joseph Fusco <josephfusco@users.noreply.github.com>
2017-07-18 09:59:22 -0400

    Update wp-skip-trash.php

--- wp-skip-trash.php
+++ wp-skip-trash.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * Plugin Name:    WP Skip Trash
- * Description:    Permanently delete items when trashed.
- * Version:        1.0
- * Author:         Joseph Fusco
- * Author URI:     http://josephfus.co/
- * License:        GPLv2 or later
- * License URI:    http://www.gnu.org/licenses/gpl-2.0.html
+ * Plugin Name:        WP Skip Trash
+ * Description:        Permanently delete items when trashed.
+ * Version:            1.0.1
+ * Author:             Joseph Fusco
+ * Author URI:         https://josephfus.co/
+ * License:            GPLv2 or later
+ * License URI:        http://www.gnu.org/licenses/gpl-2.0.html
+ * GitHub Plugin URI:  josephfusco/wp-skip-trash
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
